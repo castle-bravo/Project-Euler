c48655423720b0c9f2898c040bc551e07a375e94
Alexander Gosselin <alexandergosselin@gmail.com>
2014-10-09 14:42:39 -0700

    Added header and notes to Problem 69 solution

--- 069 - Powerful digit counts/C++/pe_069.cc
+++ 069 - Powerful digit counts/C++/pe_069.cc
@@ -1,3 +1,17 @@
+/*
+ * file:    pe_069.cc
+ * title:   Powerful digit counts
+ * author:  Alexander P. Gosselin
+ * e-mail:  alexandergosselin@gmail.com
+ * date:    October 9, 2014
+ * 
+ * math:    1. No nth power of 10 or greater can have fewer than n + 1
+ *          digits.
+ *          2. The number of digits in a number n base b is given by:
+ *            floor(log_b(n)) + 1
+ *          3. log(b^e) = e*log(b)
+ */
+
 #include <cmath>
 #include <iostream>
 using namespace std;
