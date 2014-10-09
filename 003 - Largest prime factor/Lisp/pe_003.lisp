e403602d3ef7f58e00f73a06c67957cb1993b429
Alexander Gosselin <alexandergosselin@gmail.com>
2014-10-08 19:08:15 -0700

    Problem 3 in Lisp

---
+++ 003 - Largest prime factor/Lisp/pe_003.lisp
@@ -0,0 +1,38 @@
+#|
+file:   pe_002.lisp
+title:  Even Fibonacci numbers
+author: Alexander P. Gosselin
+mail:   alexandergosselin@gmail.com
+date:   September 29, 2014
+|#
+
+(defun largest-prime-factor (n)
+  (if (< n 0)
+      (setf n (* n -1))
+    ()
+  )
+  (loop while (= (mod n 2) 0) do
+        (setf n (/ n 2))
+  )
+  (if (= n 1)
+      (return-from largest_prime-factor 2)
+    ()
+  )
+  (let ((d 1)) 
+    (loop while (/= n 1) do
+          (setf d (+ d 2))
+          (if (= (mod n d) 0)
+              (setf n (/ n d))
+            ()
+          )
+    )
+    (return-from largest-prime-factor d)
+  )
+)
+
+(defun pe_003 ()
+  (prin1 (largest-prime-factor 600851475143))
+  (write-char #\linefeed)
+)
+
+(pe_003)
