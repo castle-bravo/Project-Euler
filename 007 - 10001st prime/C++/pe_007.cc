0a0c3b04fa00c5339549baa05909edd6d34ee178
Alexander Gosselin <alexandergosselin@gmail.com>
2014-10-24 11:35:29 -0700

    improved solution to problem 7

--- 007 - 10001st prime/C++/pe_007.cc
+++ 007 - 10001st prime/C++/pe_007.cc
@@ -3,31 +3,46 @@
  * title:   10001st prime
  * author:  Alexander P. Gosselin
  * e-mail:  alexandergosselin@gmail.com
- * date:    September 13, 2014
+ * date:    October 24, 2014
+ * 
+ * math:    This solution uses the sieve of Sundaram to find the nth
+ *          prime p_n. The sieve is initialized using the fact that p_n
+ *          is less than n*(ln(n) + ln(ln(n))).
+ * 
+ *          References:
+ *          en.wikipedia.org/wiki/Sieve_of_Sundaram
+ *          en.wikipedia.org/wiki/Prime_number_theorem
+ *              #Approximations_for_the_nth_prime_number
  */
 
+#include <cmath>
 #include <iostream>
 #include <vector>
 using namespace std;
 
-const int prime_index = 10001;
+const int N = 10001; // Nth prime
 
 int main()
 {
-  vector<bool> is_prime(1, false);
-  is_prime.push_back(true);
-  int prime_count = 2;
-  for(int n = 2; true; n++) {
-    is_prime.push_back(true);
-    for(int m = 1; m*m < n; m++) {
-      if(is_prime[m] == false) continue;
-      if((2*n + 1)%(2*m + 1) == 0) {
-        is_prime[n] = (false);
+  int max = floor(N*(log(N) + log(log(N))));
+  vector<bool> marked(max/2, false);
+  for (size_t i = 1; i < marked.size(); i++) {
+    for (size_t j = 1; j <= i; j++) {
+      size_t m = i + j + 2*i*j;
+      if (m < marked.size()) {
+        marked[m] = true;
+      } else {
         break;
       }
     }
-    if(is_prime[n]) prime_count++;
-    if(prime_count == prime_index) break;
   }
-  cout << 2*is_prime.size() - 1 << endl;
+  int n = 1;
+  for (size_t m = 1; m < marked.size(); m++) {
+    if (not marked[m]) n++;
+    if (n == N) {
+      cout << 2*m + 1 << endl;
+      break;
+    }
+  }
+  return 0;
 }
